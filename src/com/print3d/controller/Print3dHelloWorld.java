e02af6d4627763297e173058f5b9cd2c93d9a2d3
Dante <d.collazzi@gmail.com>
2017-03-19 00:56:08 +0100

    impresoras3d.com

--- src/com/print3d/controller/Print3dHelloWorld.java
+++ src/com/print3d/controller/Print3dHelloWorld.java
@@ -2,7 +2,12 @@ package com.print3d.controller;
  
 
 import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.log4j.Logger;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,12 +23,40 @@ import com.print3d.scraping.Scraping;
 @Controller
 public class Print3dHelloWorld {
  
+	private final static Logger log = Logger.getLogger(Print3dHelloWorld.class);
+	private static final String STATUSOK="Conection status:OK -->";
+	private static final String STATUSKO="Conection status:KO -->";
+	
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() throws IOException{
 		
 		Scraping sc = new Scraping();
+		String url ="https://impresoras3d.com/collections/impresoras3d?page=1";
 
-		int codigo = sc.getStatusConnectionCode("https://jarroba.com/scraping-java-jsoup-ejemplos/");
+		int codigo = sc.getStatusConnectionCode(url);
+		
+		if(codigo==200){
+			log.info(STATUSOK+url);
+			Document html=sc.getHtmlDocument(url);
+			
+			
+			System.out.println("despues de coger productos");
+//			
+			
+			 // Buscar todos los precios
+            Elements precios = html.select("p.product-price");
+           // System.out.println("Número de entradas en la página inicial de Jarroba: "+entradas.size()+"\n");
+            for (Element precio : precios) {
+            		System.out.println(precio.text());
+            		
+            }
+			//entradas.forEach(action);
+            System.out.println("cantidad de precioes:"+precios.size());
+			
+			
+		}else{
+			log.info(STATUSKO+url);
+		}
 		
 		System.out.println("El codigo es:"+codigo);
 		
--- src/com/print3d/scraping/Scraping.java
+++ src/com/print3d/scraping/Scraping.java
@@ -1,17 +1,16 @@
 package com.print3d.scraping;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.springframework.http.HttpMethod;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Scraping {
+
+	private final static Logger log = Logger.getLogger(Scraping.class);
 
 	public Scraping() {
 		// TODO Auto-generated constructor stub
@@ -19,56 +18,49 @@ public class Scraping {
 
 	/**
 	 * Con esta método compruebo el Status code de la respuesta que recibo al
-	 * hacer la petición EJM: 
-	 * 200 OK 
-	 * 300 Multiple Choices 
-	 * 301 Moved Permanently
-	 * 305 Use Proxy 
-	 * 400 Bad Request 
-	 * 403 Forbidden 
-	 * 404 Not Found 
-	 * 500 Internal Server Error 
-	 * 502 Bad Gateway 
-	 * 503 Service Unavailable
+	 * hacer la petición EJM: 200 OK 300 Multiple Choices 301 Moved Permanently
+	 * 305 Use Proxy 400 Bad Request 403 Forbidden 404 Not Found 500 Internal
+	 * Server Error 502 Bad Gateway 503 Service Unavailable
 	 * 
 	 * @param url
 	 * @return Status Code
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static int getStatusConnectionCode(String url) throws IOException {
 
+		PropertyConfigurator.configure("C:\\apache-tomcat-8.0.41\\conf\\log4j.properties");
+		Response response = null;
 
-		System.out.println("Antes de conectar");
-//		System.setProperty("http.proxyHost", "10.110.8.100");
-//		System.setProperty("http.proxyPort", "8080");
-//		HttpClient client = new DefaultHttpClient();
-//		HttpGet request = new HttpGet("http://www.vogella.com");
-//		HttpResponse response = client.execute(request);
-		
-		
-		// Get the response
-//		BufferedReader rd = new BufferedReader
-//		        (new InputStreamReader(
-//		        response.getEntity().getContent()));
-//
-//		String line = "";
-//		PrintStream textView = null;
-//		
-//		while ((line = rd.readLine()) != null) {
-//		       
-//				textView.append(line);
-//		}
-		
-		System.out.println("out");
-//		System.setProperty("http.proxyHost", "10.110.8.100");
-//        System.setProperty("http.proxyPort", "8080");
-//
-//        String proxy=System.getProperty("http.proxyHost");
-//        System.out.println("proxy:"+proxy);
-//        Document doc = Jsoup.connect("http://stackoverflow.com").timeout(300000).get();
-//        System.out.println("Obtained Title: " + doc.title());
-		
-		return 200;
+		try {
+			response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).validateTLSCertificates(false).execute();
+		} catch (IOException ex) {
+			log.info("Error al conectar a:  URL: " + url);
+			log.info("ERROR: " + ex.getMessage());
+			System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
+			return -1;
+		}
+		return response.statusCode();
+	}
+
+	/**
+	 * Con este método devuelvo un objeto de la clase Document con el contenido
+	 * del HTML de la web que me permitirá parsearlo con los métodos de la
+	 * librelia JSoup
+	 * 
+	 * @param url
+	 * @return Documento con el HTML
+	 */
+	public static Document getHtmlDocument(String url) {
+
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).validateTLSCertificates(false).get();
+
+		} catch (IOException ex) {
+			log.error("Excepción al obtener el HTML de la página" + ex.getMessage());
+			System.out.println("Excepción al obtener el HTML de la página" + ex.getMessage());
+		}
+		return doc;
 	}
 
 }
